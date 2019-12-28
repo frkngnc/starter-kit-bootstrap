9bc0edea3cc77e8eef984daf6a558a92263d4fcc
Furkan <furkangenc@yandex.com>
2019-12-28 04:36:22 +0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,113 @@
+const gulp = require('gulp');
+const del = require('del');
+const sass = require('gulp-sass');
+const uglify = require('gulp-uglify');
+const browserSync = require('browser-sync').create();
+const pug = require('gulp-pug');
+const imagemin = require('gulp-imagemin');
+const concat = require('gulp-concat');
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+const sassGlob = require('gulp-sass-glob');
+
+// Import img
+function img() {
+  return gulp
+    .src('src/img/**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('dist/img'))
+    .pipe(browserSync.stream());
+}
+
+// Import fonts
+function fonts() {
+  return gulp
+    .src('src/fonts/**/*')
+    .pipe(gulp.dest('dist/fonts'))
+    .pipe(browserSync.stream());
+}
+
+// Import HTTML
+function html() {
+  return gulp
+    .src('./src/pug/pages/*.pug')
+    .pipe(
+      pug({
+        pretty: true,
+      }),
+    )
+    .pipe(gulp.dest('./dist'))
+    .pipe(browserSync.reload({ stream: true }));
+}
+
+// Import CSS
+function css() {
+  return gulp
+    .src('./src/sass/*.scss')
+    .pipe(sassGlob())
+    .pipe(sass())
+    .pipe(
+      autoprefixer({
+        overrideBrowserslist: ['> 0.1%'],
+        cascade: false,
+      }),
+    )
+    .pipe(cleanCSS({ compatibility: 'ie8' }))
+    .pipe(gulp.dest('./dist/css'))
+    .pipe(browserSync.stream());
+}
+
+// Import JS files
+let jsFiles = [
+  './src/libs/js/jquery.min.js',
+  './src/libs/bootstrap/js/bootstrap.min.js',
+  './src/js/script.js', // always last
+];
+function js() {
+  return gulp
+    .src(jsFiles)
+    .pipe(concat('main.js'))
+    .pipe(
+      uglify({
+        toplevel: true,
+      }),
+    )
+    .pipe(gulp.dest('./dist/js'))
+    .pipe(browserSync.stream());
+}
+
+// Live server
+function watch() {
+  browserSync.init({
+    server: {
+      baseDir: './dist',
+    },
+    notify: false,
+    // tunnel: true
+  });
+  gulp.watch('./src/pug/**/*.pug', html);
+  gulp.watch('./src/fonts/**/*', fonts);
+  gulp.watch('./src/img/**/*', img);
+  gulp.watch('./src/sass/*.scss', css);
+  gulp.watch('./src/js/*.js', js);
+  gulp.watch('dist/*.html').on('change', browserSync.reload);
+}
+
+// Clean build folder
+function clean() {
+  return del(['dist/*']);
+}
+
+// TASKS
+gulp.task('css', css);
+gulp.task('img', img);
+gulp.task('js', js);
+gulp.task('watch', watch);
+gulp.task('clean', clean);
+gulp.task('html', html);
+gulp.task('fonts', fonts);
+
+gulp.task('build', gulp.series('css', 'js', 'html', 'img', 'fonts'));
+
+gulp.task('dev', gulp.series('build', 'watch'));
+
---
+++ src/js/script.js
@@ -0,0 +1,199 @@
+//AOS
+  AOS.init();
+
+var swiper = new Swiper('.swiper1', {
+  slidesPerView: 2,
+  spaceBetween: 0,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+    
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      
+    },
+    768: {
+      slidesPerView: 4,
+    
+    },
+    1024: {
+      slidesPerView: 10,
+      
+    },
+  }
+});
+
+var swiper = new Swiper('.swiper2', {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  navigation: {
+    nextEl: '.next2',
+    prevEl: '.prev2',
+   
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+      
+    },
+    768: {
+      slidesPerView: 2,
+    
+    },
+    1024: {
+      slidesPerView: 3,
+      
+    },
+  }
+});
+
+        
+        
+
+
+$(function() {
+  // ------------------------------------------------------- //
+  // Multi Level dropdowns
+  // ------------------------------------------------------ //
+  $("ul.dropdown-menu [data-toggle='dropdown']").on("click", function(event) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    $(this).siblings().toggleClass("show");
+
+
+    if (!$(this).next().hasClass('show')) {
+      $(this).parents('.dropdown-menu').first().find('.show').removeClass("show");
+    }
+    $(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', function(e) {
+      $('.dropdown-submenu .show').removeClass("show");
+    });
+
+  });
+});
+
+
+
+//Particle JS
+particlesJS('particles-js',
+  {
+    "particles": {
+      "number": {
+        "value": 60,
+        "density": {
+          "enable": true,
+          "value_area": 700
+        }
+      },
+      "color": {
+        "value": "#fff"
+      },
+      "shape": {
+        "type": "circle",
+        "stroke": {
+          "width": 0,
+          "color": "#000000"
+        },
+        "polygon": {
+          "nb_sides": 5
+        },
+        "image": {
+          "src": "img/github.svg",
+          "width": 100,
+          "height": 100
+        }
+      },
+      "opacity": {
+        "value": 0.2,
+        "random": false,
+        "anim": {
+          "enable": false,
+          "speed": 1,
+          "opacity_min": 0.1,
+          "sync": false
+        }
+      },
+      "size": {
+        "value": 5,
+        "random": true,
+        "anim": {
+          "enable": false,
+          "speed": 30,
+          "size_min": 0.1,
+          "sync": false
+        }
+      },
+      "line_linked": {
+        "enable": true,
+        "distance": 150,
+        "color": "#ffffff",
+        "opacity": 0.2,
+        "width": 1
+      },
+      "move": {
+        "enable": true,
+        "speed": 6,
+        "direction": "none",
+        "random": false,
+        "straight": false,
+        "out_mode": "out",
+        "attract": {
+          "enable": false,
+          "rotateX": 600,
+          "rotateY": 1200
+        }
+      }
+    },
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": true,
+          "mode": "repulse"
+        },
+        "onclick": {
+          "enable": true,
+          "mode": "push"
+        },
+        "resize": true
+      },
+      "modes": {
+        "grab": {
+          "distance": 400,
+          "line_linked": {
+            "opacity": 1
+          }
+        },
+        "bubble": {
+          "distance": 400,
+          "size": 40,
+          "duration": 2,
+          "opacity": 8,
+          "speed": 3
+        },
+        "repulse": {
+          "distance": 200
+        },
+        "push": {
+          "particles_nb": 4
+        },
+        "remove": {
+          "particles_nb": 2
+        }
+      }
+    },
+    "retina_detect": true,
+    "config_demo": {
+      "hide_card": false,
+      "background_color": "#ccc",
+      "background_image": "",
+      "background_position": "50% 50%",
+      "background_repeat": "no-repeat",
+      "background_size": "cover"
+    }
+  }
+
+);
+
